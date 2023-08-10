82334efd94810cbaf81c42469213c9eeb877e3c8
pica69co <oscarwarrieta@gmail.com>
2023-08-10 13:06:16 -0500

    feature/threadcards

---
+++ components/cards/ThreadCards.tsx
@@ -0,0 +1,37 @@
+interface Props {
+    id: string; 
+    currentUserId: string; 
+    parentId: string | null; 
+    content: string; 
+    author:{
+        name: string;
+        image: string;
+        id: string;
+    } 
+    community:{
+        id: string;
+        name: string;
+        image: string;
+    } | null;
+    createdAt: string; 
+    comments:{
+        author:{
+            image: string;
+        }
+    }[]
+    isComment?: boolean;
+}
+
+const ThreadCard = ({
+    id, currentUserId, parentId, content, author, community, createdAt, comments
+} : Props) => {
+    return (
+        <article className="flex w-flex flex-col rounded-xl bbg-dark-2 p7">
+            <h2 className="text-small-regular text-light-2">
+                {content}
+            </h2>
+        </article>
+    )
+}
+
+export default ThreadCard;
--- lib/actions/thread.actions.ts
+++ lib/actions/thread.actions.ts
@@ -13,7 +13,7 @@ interface Params {
 }
 
 export async function createThread({ text, author, communityId, path}: Params){
-    try {
+   // try {
         
         connectDb()
     
@@ -29,8 +29,48 @@ export async function createThread({ text, author, communityId, path}: Params){
     
         revalidatePath(path)
 
-    } catch (error: any) {
-        throw new Error(`Failed to create or update thread: ${error.message}`)
-    }
+   // } catch (error: any) {
+   //     throw new Error(`Failed to create or update thread: ${error.message}`)
+  //  }
     
 }
+
+export async function fetchPosts(pageNumber = 1, pageSize = 20) {
+    connectDb();
+  
+    // Calculate the number of posts to skip based on the page number and page size.
+    const skipAmount = (pageNumber - 1) * pageSize;
+  
+    // Create a query to fetch the posts that have no parent (top-level threads) (a thread that is not a comment/reply).
+    const postsQuery = Thread.find({ parentId: { $in: [null, undefined] } })
+      .sort({ createdAt: "desc" })
+      .skip(skipAmount)
+      .limit(pageSize)
+      .populate({
+        path: "author",
+        model: User,
+      })
+    //   .populate({
+    //     path: "community",
+    //     model: Community,
+    //   })
+      .populate({
+        path: "children", // Populate the children field
+        populate: {
+          path: "author", // Populate the author field within children
+          model: User,
+          select: "_id name parentId image", // Select only _id and username fields of the author
+        },
+      });
+  
+    // Count the total number of top-level posts (threads) i.e., threads that are not comments.
+    const totalPostsCount = await Thread.countDocuments({
+      parentId: { $in: [null, undefined] },
+    }); // Get the total count of posts
+  
+    const posts = await postsQuery.exec();
+  
+    const isNext = totalPostsCount > skipAmount + posts.length;
+  
+    return { posts, isNext };
+  }
